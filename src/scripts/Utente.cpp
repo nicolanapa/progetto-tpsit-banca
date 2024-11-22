d409aec0c314bd8d0b5ca4e24d58d02e6c3206bc
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-22 19:36:23 +0100

    Added portafoglio variable to keep locally the money
    Added prototyping of Utente() and add100()

--- src/scripts/Utente.cpp
+++ src/scripts/Utente.cpp
@@ -1,1 +1,5 @@
 #include "Utente.h"
+
+Utente::Utente(double portafoglio) {}
+
+void Utente::add100() {}
--- src/scripts/Utente.h
+++ src/scripts/Utente.h
@@ -1,3 +1,11 @@
 #pragma once
 
-class Utente {};
+class Utente {
+	private:
+	double portafoglio;
+
+	public:
+	Utente(double portafoglio);
+
+	void add100();
+};
