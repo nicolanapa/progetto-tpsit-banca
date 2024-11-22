1076c198061d4b5166867eeedc7ed00204a81972
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-22 20:46:33 +0100

    Added code to take/post money and added getSoldi to return soldi

--- src/scripts/Banca.cpp
+++ src/scripts/Banca.cpp
@@ -1,15 +1,32 @@
 #include "Banca.h"
 
-Banca::Banca(double soldi, Utente utente) {}
+Banca::Banca(double soldi, Utente utente) {
+	this->soldi = soldi;
+	this->utente = utente;
+}
 
-void Banca::deposito() {}
+void Banca::deposito(double somma) {
+	if (this->utente.getPortafoglio() - somma >= 0) {
+		this->soldi += somma;
+		this->utente.addToPortafoglio(-somma);
+	} else {
+		cout << "Non hai abbastanza soldi per depositare" << endl;
+	}
+}
 
-void Banca::prelievo() {}
+void Banca::prelievo(double sottrazione) {
+	if (this->soldi - sottrazione >= 0) {
+		this->soldi -= sottrazione;
+		this->utente.addToPortafoglio(sottrazione);
+	} else {
+		cout << "Non hai abbastanza soldi per fare il prelievo" << endl;
+	}
+}
 
 void Banca::investimento() {}
 
 void Banca::avanzamento() {}
 
 double Banca::getSoldi() {
-	return 0.0;
+	return this->soldi;
 }
