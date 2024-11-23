9597b62f1c23cad98ca7362ea6ab6d6afb89fd2a
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-23 23:10:05 +0100

    Continued avanzamento() and added getTime() that prints current time

--- src/scripts/Data.cpp
+++ src/scripts/Data.cpp
@@ -6,4 +6,28 @@ Data::Data(int giorno, int mese, int anno) {
 	this->anno = anno;
 }
 
-void Data::avanzamento(int giorno, int mese, int anno) {}
+void Data::avanzamento(int giorno, int mese, int anno) {
+	if (this->giorno + giorno > 31) {
+		int tempGiorno = (this->giorno + giorno) / 31;
+
+		this->giorno = (this->giorno + giorno % 365) % 30;
+		this->mese += tempGiorno;
+	} else {
+		this->giorno += giorno;
+	}
+
+	if (this->mese + mese > 12) {
+		int tempMese = (this->mese + mese) / 12;
+
+		this->mese = (this->mese + mese) - (tempMese * 12);
+		this->anno += tempMese;
+	} else {
+		this->mese += mese;
+	}
+
+	this->anno += anno;
+}
+
+void Data::getTime() {
+	cout << giorno << "/" << mese << "/" << anno << endl;
+}
--- src/scripts/Data.h
+++ src/scripts/Data.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
 
 class Data {
 	private:
@@ -10,4 +13,6 @@ class Data {
 	Data(int giorno = 1, int mese = 1, int anno = 2024);
 
 	void avanzamento(int giorno, int mese, int anno);
+
+	void getTime();
 };
