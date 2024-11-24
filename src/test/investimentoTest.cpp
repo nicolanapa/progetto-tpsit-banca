cb4c74ff8889752e82071365036c11f9289b3e30
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-24 09:22:39 +0100

    Added some tests about 'durata' and 'rischio'

--- src/test/investimentoTest.cpp
+++ src/test/investimentoTest.cpp
@@ -2,9 +2,23 @@
 #include "../scripts/Banca.h"
 
 void investimentoTest() {
-	Banca breachDiSicurezza(100, Utente(), Data(23, 11));
+	Banca breachDiSicurezza(10000, Utente(), Data(23, 11));
 
 	breachDiSicurezza.deposito(50);
 
-	cout << breachDiSicurezza.getSoldi() << endl;
+	breachDiSicurezza.investimento(100, "breve");
+	breachDiSicurezza.investimento(50, "media");
+	breachDiSicurezza.investimento(50, "lunga");
+
+	breachDiSicurezza.investimento(120, "breve", "alto");
+	breachDiSicurezza.investimento(50, "media", "alto");
+	breachDiSicurezza.investimento(7000, "lunga", "alto");
+
+	if (breachDiSicurezza.getSoldi() <= 0) {
+		breachDiSicurezza.investimento(100, "breve");
+
+		breachDiSicurezza.deposito(1500);
+
+		cout << breachDiSicurezza.getSoldi() << endl;
+	}
 }
