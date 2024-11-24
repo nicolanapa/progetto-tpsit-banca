322c261c53c2aefe7b7116599767caa0729866a0
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-24 11:34:50 +0100

    Added Banca aiutoRichiesto variable to test avanzamento()

--- src/test/investimentoTest.cpp
+++ src/test/investimentoTest.cpp
@@ -17,8 +17,42 @@ void investimentoTest() {
 	if (breachDiSicurezza.getSoldi() <= 0) {
 		breachDiSicurezza.investimento(100, "breve");
 
+		cout << breachDiSicurezza.getPortafoglio() << endl;
+
 		breachDiSicurezza.deposito(1500);
 
 		cout << breachDiSicurezza.getSoldi() << endl;
+		cout << breachDiSicurezza.getPortafoglio() << endl;
 	}
+
+	Banca aiutoRichiesto(50);
+
+	cout << aiutoRichiesto.getPortafoglio() << endl;
+
+	aiutoRichiesto.avanzamento(0, 2);
+
+	cout << aiutoRichiesto.getPortafoglio() << endl;
+
+	aiutoRichiesto.avanzamento(0, 0, 1);
+
+	cout << aiutoRichiesto.getPortafoglio() << endl;
+
+	aiutoRichiesto.avanzamento(0, 2, 1);
+
+	cout << aiutoRichiesto.getPortafoglio() << endl;
+
+	aiutoRichiesto.avanzamento(62);
+
+	cout << aiutoRichiesto.getPortafoglio() << endl;
+
+	aiutoRichiesto.avanzamento(62, 24, 2);
+
+	cout << aiutoRichiesto.getPortafoglio() << endl;
+
+	aiutoRichiesto.deposito(7000);
+	aiutoRichiesto.investimento(7000, "lunga", "alto");
+
+	cout << aiutoRichiesto.getPortafoglio() << endl;
+
+	aiutoRichiesto.investimento(100, "lunga", "alto");
 }
