bbfad659a54f15172c189daa09895b6773c4bad6
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-24 09:22:06 +0100

    Added algorithm to calculate earned money

--- src/scripts/Banca.cpp
+++ src/scripts/Banca.cpp
@@ -25,8 +25,8 @@ void Banca::prelievo(double sottrazione) {
 	}
 }
 
-void Banca::investimento(string durata, string rischioTeorico) {
-	if (this->soldi <= 0) {
+void Banca::investimento(double soldi, string durata, string rischioTeorico) {
+	if (this->soldi <= 0 || soldi <= 0 || this->soldi - soldi < 0) {
 		cout << "Nessun investimento disponibile: ";
 		cout << "Conto inferiore o uguale a 0" << endl;
 
@@ -39,30 +39,69 @@ void Banca::investimento(string durata, string rischioTeorico) {
 	if (durata == "breve") {
 		this->avanzamento(0, 1, 0);
 
-		guadagno = 1.0002;
+		guadagno = 1.05;
 	} else if (durata == "media") {
 		this->avanzamento(0, 6, 0);
 
-		guadagno = 1.002;
+		guadagno = 1.25;
 	} else if (durata == "lunga") {
 		this->avanzamento(0, 0, 1);
 
-		guadagno = 1.2;
+		guadagno = 1.4;
 	}
 
 	if (rischioTeorico == "basso") {
-		rischio = 0.75;
+		rischio = 1;
 	} else if (rischioTeorico == "medio") {
 		rischio = 1.5;
 	} else if (rischioTeorico == "alto") {
 		rischio = 3;
 	}
 
+	double randomValue = (rand() % 8000) * rischio;
+	double investimentoFinale = (soldi * guadagno) - soldi;
+
 	cout << "Calcolazione perdita o guadagno finale..." << endl;
-	if (/*rand()*/1) {
+	// cout << "RAND " << randomValue << endl << endl;
+
+	if (randomValue < 4500) {
 		cout << "Guadagno!" << endl;
+		cout << "Hai guadagnato ";
+
+		if (rischioTeorico == "basso") {
+			cout << investimentoFinale * 1.5 << " euro" << endl;
+
+			this->soldi += investimentoFinale;
+		} else if (rischioTeorico == "medio") {
+			cout << investimentoFinale * 2.5 << " euro" << endl;
+
+			this->soldi += investimentoFinale * 2.5;
+		} else if (rischioTeorico == "alto") {
+			cout << investimentoFinale * 4 << " euro" << endl;
+
+			this->soldi += investimentoFinale * 4;
+		}
+
+		cout << "Conto attuale: " << this->getSoldi() << " euro" << endl;
 	} else {
 		cout << "Perdita!" << endl;
+		cout << "Hai perso ";
+
+		if (rischioTeorico == "basso") {
+			cout << investimentoFinale * 1.5 << " euro" << endl;
+
+			this->soldi -= investimentoFinale;
+		} else if (rischioTeorico == "medio") {
+			cout << investimentoFinale * 2.5 << " euro" << endl;
+
+			this->soldi -= investimentoFinale * 2.5;
+		} else if (rischioTeorico == "alto") {
+			cout << investimentoFinale * 4 << " euro" << endl;
+
+			this->soldi -= investimentoFinale * 4;
+		}
+
+		cout << "Conto attuale: " << this->getSoldi() << " euro" << endl;
 	}
 }
 
